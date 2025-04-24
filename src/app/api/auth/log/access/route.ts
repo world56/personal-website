7f8c6768cabcdd2fc03bac9cb412183731cad22b
world56 <world56@qq.com>
2025-04-24 14:46:54 +0800

    refactor: 时区关键字配置化

--- src/app/api/auth/log/access/route.ts
+++ src/app/api/auth/log/access/route.ts
@@ -1,10 +1,12 @@
 import { NextResponse } from "next/server";
 import { prisma, cacheable } from "@/lib/db";
 
+import { KEY_TIME_ZONE } from "@/config/common";
+
 import { ENUM_COMMON } from "@/enum/common";
 
 export async function DELETE() {
   await prisma.log.deleteMany({ where: { type: ENUM_COMMON.LOG.ACCESS } });
-  cacheable.delete("time-zone");
+  cacheable.delete(KEY_TIME_ZONE);
   return NextResponse.json(true);
 }
